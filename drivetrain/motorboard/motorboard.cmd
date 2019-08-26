220ee4b2ba0c4c68b315c6027530b23b9b7fc783
Phil Underwood <beardydoc@gmail.com>
2019-08-26 10:58:47 +0100

    Start developing motor controller

---
+++ drivetrain/motorboard/motorboard.cmd
@@ -0,0 +1,61 @@
+# Pin name action command file
+
+# Start of element B1
+ChangePinName(B1, 2, -)
+ChangePinName(B1, 1, +)
+
+# Start of element CONN3
+ChangePinName(CONN3, 11, 11)
+ChangePinName(CONN3, 9, 9)
+ChangePinName(CONN3, 12, 12)
+ChangePinName(CONN3, 10, 10)
+ChangePinName(CONN3, 7, 7)
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 8, 8)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element U2
+ChangePinName(U2, 7, Out2)
+ChangePinName(U2, 6, Vs)
+ChangePinName(U2, 5, In2)
+ChangePinName(U2, 4, Gnd)
+ChangePinName(U2, 3, In1)
+ChangePinName(U2, 2, EF)
+ChangePinName(U2, 1, Out1)
+
+# Start of element U1
+ChangePinName(U1, 7, Out2)
+ChangePinName(U1, 6, Vs)
+ChangePinName(U1, 5, In2)
+ChangePinName(U1, 4, Gnd)
+ChangePinName(U1, 3, In1)
+ChangePinName(U1, 2, EF)
+ChangePinName(U1, 1, Out1)
---
+++ drivetrain/motorboard/packages/TLE2602.fp
@@ -0,0 +1,14 @@
+Element ["" "TLE2602" "" "" 0 0 0 0 0 100 ""] (
+Pin [-3.8100000000000005mm -1.27mm 2.0mm 0.2mm 2.2mm 1.0mm "Out1" "1" "square"]
+Pin [-1.2700000000000005mm -1.27mm 2.0mm 0.2mm 2.2mm 1.0mm "In1" "3" ""]
+Pin [1.2699999999999996mm -1.27mm 2.0mm 0.2mm 2.2mm 1.0mm "In2" "5" ""]
+Pin [3.8100000000000005mm -1.27mm 2.0mm 0.2mm 2.2mm 1.0mm "Out2" "7" ""]
+Pin [-2.5400000000000005mm 1.27mm 2.0mm 0.2mm 2.2mm 1.0mm "EF" "2" ""]
+Pin [0.0mm 1.27mm 2.0mm 0.2mm 2.2mm 1.0mm "Gnd" "4" ""]
+Pin [2.539999999999999mm 1.27mm 2.0mm 0.2mm 2.2mm 1.0mm "Vs" "6" ""]
+ElementLine [-5.11mm 2.57mm 5.110000000000001mm 2.57mm 0.2mm]
+ElementLine [-5.11mm -2.57mm 5.110000000000001mm -2.57mm 0.2mm]
+ElementLine [-5.11mm 2.57mm -5.11mm -2.57mm 0.2mm]
+ElementLine [5.110000000000001mm 2.57mm 5.110000000000001mm -2.57mm 0.2mm]
+)
+
