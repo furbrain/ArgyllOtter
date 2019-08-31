83333ef3d68be444edefa7ece618f8dec489ab80
Phil Underwood <beardydoc@gmail.com>
2019-08-31 20:37:43 +0100

    Redesign controller and driver boards

---
+++ drivetrain/pcbs/motorboard.cmd
@@ -0,0 +1,74 @@
+# Pin name action command file
+
+# Start of element M2
+ChangePinName(M2, 2, 2)
+ChangePinName(M2, 1, 1)
+
+# Start of element M1
+ChangePinName(M1, 2, 2)
+ChangePinName(M1, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element J1
+ChangePinName(J1, 9, 9)
+ChangePinName(J1, 7, 7)
+ChangePinName(J1, 5, 5)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 8, 8)
+ChangePinName(J1, 6, 6)
+ChangePinName(J1, 4, 4)
+ChangePinName(J1, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element CONN1
+ChangePinName(CONN1, 14, 14)
+ChangePinName(CONN1, 13, 13)
+ChangePinName(CONN1, 12, 12)
+ChangePinName(CONN1, 11, 11)
+ChangePinName(CONN1, 10, 10)
+ChangePinName(CONN1, 9, 9)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element B1
+ChangePinName(B1, 2, -)
+ChangePinName(B1, 1, +)
+
+# Start of element U2
+ChangePinName(U2, 7, Out2)
+ChangePinName(U2, 6, Vs)
+ChangePinName(U2, 5, In2)
+ChangePinName(U2, 4, Gnd)
+ChangePinName(U2, 3, In1)
+ChangePinName(U2, 2, EF)
+ChangePinName(U2, 1, Out1)
+
+# Start of element U1
+ChangePinName(U1, 7, Out2)
+ChangePinName(U1, 6, Vs)
+ChangePinName(U1, 5, In2)
+ChangePinName(U1, 4, Gnd)
+ChangePinName(U1, 3, In1)
+ChangePinName(U1, 2, EF)
+ChangePinName(U1, 1, Out1)
