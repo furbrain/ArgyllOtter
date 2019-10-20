bcb32601d176512da1f365f05d412ae7ad0931a9
Phil Underwood <beardydoc@gmail.com>
2019-10-20 11:55:13 +0100

    Start work on servo calibration

--- software/shooter.py
+++ software/shooter.py
@@ -5,8 +5,12 @@ import simple_pid
 import servo
 import orientation
 import gpiozero
+import numpy as np
 
 BMP388_ADDRESS = 0x77
+UP_CAL_FILE = "/home/pi/calibration_up.np"
+DOWN_CAL_FILE = "/home/pi/calibration_down.np"
+CAL_RANGE = (-30,70,5)
 
 class Pressure:
     def __init__(self, bus = None, address = BMP388_ADDRESS):
@@ -33,15 +37,38 @@ class Barrel:
         self.pid = simple_pid.PID(0.3, 1.5, 0.0001,
                                   output_limits = (70, 145) , 
                                   sample_time=0.05)
+        try:
+            f = open(UP_CAL_FILE)
+        except IOError:
+            rng = range(*CAL_RANGE)
+            rv = reversed(rng)
+            self.cal_up = np.stack(rng,rng).T
+            self.cal_down = np.stack(rv,rv).T
+        else:
+            with f as f:
+                self.cal_up = np.load(f)
 
     def set_angle(self, angle):
         self.pid.setpoint = angle
 
     def update(self):
         angle = self.orientation.get_angle()
-        output = 215 - self.pid(angle)
+        output = self.pid(angle)
         self.servo.set_pos(output)
         return (angle, output)
+        
+    def calibrate(self):
+        with open("/home/pi/calibration_up.csv", "w") as f:
+            for i in range(-30, 70, 5):
+                self.serve.set_pos(i)
+                time.sleep(1)
+                f.write('{:3d},{:6.2f}'.format(i, self.orientation.get_angle()))
+        with open("/home/pi/calibration_down.csv", "w") as f:
+            for i in reversed(range(-30, 70, 5)):
+                self.serve.set_pos(i)
+                time.sleep(1)
+                f.write('{:3d},{:6.2f}'.format(i, self.orientation.get_angle()))
+                
         
 def Pointer():
     return gpiozero.LED(22)
