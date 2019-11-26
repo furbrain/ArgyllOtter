851dd5f1e18447d9b1d074ff5ac738903544f030
Phil Underwood <beardydoc@gmail.com>
2019-11-26 21:30:29 +0000

    Add debounce delay for button press

--- software/hardware/encoder.py
+++ software/hardware/encoder.py
@@ -2,33 +2,38 @@
 
 from gpiozero import Button
 import logging
+import time
 from util import logged
 
 
 class Encoder():
-    def __init__(self, pins, on_changed, on_pressed):
+    def __init__(self, on_changed, on_pressed, pins=None):
+        if pins is None:
+            pins = (26, 6, 19)
         self.on_changed = on_changed
         self.on_pressed = on_pressed
         self.clock = Button(pins[0])
         self.data = Button(pins[1])
         self.switch = Button(pins[2])
-        self.pos = 0
         self.clock.when_pressed = self.movement
         self.switch.when_pressed = self.pressed    
+        self.last_press = time.time()
 
     @logged
     def movement(self):
         if self.data.is_pressed:
-            self.pos +=1
+            self.on_changed(True)
         else:
-            self.pos -=1
-        self.on_changed(self.pos)
+            self.on_changed(False)
 
     @logged
     def pressed(self):
-        self.on_pressed()
+        now = time.time()
+        if now > (self.last_press+0.5):
+            self.on_pressed()
+            self.last_press = now
         
 
 if __name__=="__main__":
-    enc = Encoder((19,13,26), print, lambda: print("Button"))
+    enc = Encoder(print, lambda: print("Button"))
     input("press Enter to finish")
