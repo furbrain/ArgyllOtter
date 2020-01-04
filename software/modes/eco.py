7f7468075b03d1de49fc84bded80daa91ee74a37
Phil Underwood <beardydoc@gmail.com>
2020-01-04 22:32:26 +0000

    Start work on eco disaster training

--- software/modes/eco.py
+++ software/modes/eco.py
@@ -1,39 +1,106 @@
 from modes import mode
-from hardware import Camera, Display, Laser, Grabber
 import asyncio
 from compute import vision
+import cv2
+import numpy as np
 
+TURN_SPEED = 50
+DRIVE_SPEED = 400
 
 class Retrieve(mode.Mode):
-    def on_start(self):
-        self.camera = Camera()
-        self.display = Display()
-        self.laser = Laser()
-        self.grabber = Grabber()
-
+    HARDWARE = ('drive','camera','laser','grabber')
+        
     async def run(self):
         self.grabber.open()
         await asyncio.sleep(1)
         image = self.camera.get_image()
-        contour = await spawn(vision.find_biggest_contour(image, colour_name="red"))
-        if contour is not None:
+        cv2.imwrite("fred.jpg", image)
+        positions = await vision.find_objects(self.camera, "red", 56)
+        print(positions)
+        nearest = min(positions, key = lambda x: x[1], default=(0,0))
+        print(nearest)
+        await self.drive.spin(nearest[0], 20)
+        dist = await self.laser.get_distance()
+        print(dist)
+        await self.drive.a_goto(400, dist-50)
+        print(await self.laser.get_distance())
+        await asyncio.sleep(2)
+
+class Process(mode.Mode):
+    HARDWARE = ('drive','camera','laser','grabber')
+    
+    def on_start(self):
+        self.pos =np.array((1100,1800), dtype="float64")
+        self.azimuth = 0 #"north", in degrees
+        self.barrel_map = []
         
-	        # find the largest contour in the mask, then use
-	        # it to compute the minimum enclosing circle and
-	        # centroid
-	        x_min = min(contour[:,:,0])[0]
-	        x_max = max(contour[:,:,0])[0]
-	        y_max = max(c[:,:,1])[0]
-	        pos = (x_min+x_max/2) - self.camera.calibration.zero_degree_pixel
-	        angle = pos * self.camera.calibration.degrees_per_pixel
-	        await self.drive.spin(angle, 100)
-	        dist = await self.laser.get_distance(Laser.MEDIUM)
-	        await self.drive.a_goto(200,dist-10)
-	        self.grabber.close()
-	        await asyncio.sleep(0.6)
-	        await self.drive.a_goto(200,-dist)
-	        self.grabber.open()
-	        await asyncio.sleep(0.6)
+    def get_coeffs(self):
+        radians = self.azimuth * np.pi / 180
+        return np.array((np.sin(radians), np.cos(radians)))
+        
+    async def turn(self, angle, speed=TURN_SPEED):
+        await self.drive.spin(angle, TURN_SPEED)
+        self.azimuth += angle
+        print(self.azimuth)
+        
+    async def goto(self, distance, speed=DRIVE_SPEED):
+        if distance < 0:
+            speed = -speed
+        await self.drive.a_goto(speed, distance)
+        self.pos += self.get_coeffs() * distance
+        
+    async def find_barrels(self):
+        tasks = [vision.find_objects(self.camera, x, 56) for x in ("red","green")]
+        reds, greens = await asyncio.gather(*tasks)
+        barrels = [(angle, pos, "red") for angle, pos in reds]
+        barrels += [(angle, pos, "green") for angle, pos in greens]
+        return barrels
+        
+    async def check_centred(self, colour):
+        image = self.camera.get_image()
+        positions = await vision.find_objects(self.camera, colour, 56)
+        angles = [(abs(x[0]), x[0]) for x in positions]
+        central = sorted(angles)[0]
+        if central[0]<1:
+            return 0
         else:
-            self.display.draw_text("Nuffin")
-        self.grabber.off()
+            return central[1] 
+         
+    async def create_map(self):
+        await self.turn(-90)
+        targeted = False
+        first = True
+        #find most leftward barrels
+        while self.azimuth < 90:
+            barrels = await self.find_barrels()
+            if targeted:
+                barrels = list(filter(lambda x: x[0] > 2, barrels))
+            elif not first:
+                barrels = list(filter(lambda x: x[0] > -13, barrels))
+            first = False
+            if len(barrels)>0:
+                target = sorted(barrels)[0]
+                await self.turn(target[0])
+                while True:
+                    distance, adjustment = await asyncio.gather(self.laser.get_distance(), self.check_centred(target[2]))
+                    if adjustment == 0:
+                        break
+                    await self.turn(adjustment, speed=TURN_SPEED/4)
+                targeted = True
+                pos = self.pos + self.get_coeffs() * distance
+                self.barrel_map.append((pos, target[2]))
+            else:
+                await self.turn(15)
+                targeted = False
+        print(self.barrel_map) 
+        
+    async def run(self):
+        #map all the barrels
+        await self.goto(-100.0)
+        await self.create_map()
+        #while there are barrels:
+         #find nearest barrel
+         #go to it
+         #grab it
+         #find path to destination
+         #go there
