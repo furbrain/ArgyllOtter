4a5871ac02a5ffac8ea538b64ebcff73ee898982
Phil Underwood <beardydoc@gmail.com>
2020-02-28 22:05:58 +0000

    Update modes

---
+++ software/modes/__init__.py
@@ -0,0 +1,7 @@
+from .eco import Process
+from .escape import Learn, Walk, Run
+from .lava import Lava
+from .manual import Manual
+from .mode import Mode, Interactive
+from .shooter import Shooter
+from .minesweeper import MineSweeper
