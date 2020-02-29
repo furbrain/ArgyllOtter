cf249054ff0ce3aa0b95b01e8f69cce7c1a06844
Phil Underwood <beardydoc@gmail.com>
2020-02-29 17:07:14 +0000

    Make stabber a context manager

--- software/hardware/stabber.py
+++ software/hardware/stabber.py
@@ -1,3 +1,5 @@
+import time
+
 import settings
 import hardware.servo
 
@@ -10,8 +12,15 @@ class StabberPosition(settings.Settings):
 
 class Stabber:
     def __init__(self):
-        self.servo = hardware.servo.Servo(pin="0")
+        self.servo = hardware.servo.Servo(pin="1")
         self.positions = StabberPosition()
+
+    def __enter__(self):
+        self.stab()
+        time.sleep(0.1)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release()
 
     def stab(self):
         self.servo.set_pos(self.positions.stab)
