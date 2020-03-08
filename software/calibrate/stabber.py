5fd050e7d45de9e54169489320b204046a5c67e0
Phil Underwood <beardydoc@gmail.com>
2020-03-08 16:31:53 +0000

    bug fix

--- software/calibrate/stabber.py
+++ software/calibrate/stabber.py
@@ -20,8 +20,9 @@ class StabberCal(modes.Interactive):
         self.stabber.servo.set_pos(self.angle)
 
     async def run(self):
+        self.stabber.servo.set_pos(0)
         cal = self.stabber.positions
-        self.display.draw_text("Open")
+        self.display.draw_text("Stab")
         await self.wait_for_button()
         cal.stab = self.angle
         self.display.draw_text("Release")
